cdb63b41f743628edbe7a196f61f0f9d6131882f
Saman Merchant <samanzmerchant@gmail.com>
2024-11-27 16:14:44 -0600

    logout endpoint

--- controllers/auth.js
+++ controllers/auth.js
@@ -57,3 +57,12 @@ export const login = async (req, res) => {
         return res.status(400).send("Error. Try Again.")
     }
 };
+
+export const logout = async (req, res) => {
+    try {
+        res.clearCookie("token");
+        return res.json({ message: "Signout success" });
+    } catch (err) {
+        console.log(err);
+    }
+}
