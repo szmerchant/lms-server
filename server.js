906b8394f123f53ef8b4e7a0e7b99a76dca80fd0
Saman Merchant <samanzmerchant@gmail.com>
2024-11-27 19:01:23 -0600

    CSRF protection

--- server.js
+++ server.js
@@ -2,10 +2,14 @@ import express from "express";
 import cors from "cors";
 import { readdirSync } from "fs";
 import mongoose from "mongoose";
+import csrf from "csurf";
+import cookieParser from "cookie-parser";
 import morgan from "morgan";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+const csrfProtection = csrf({ cookie: true });
 
 // create express app
 const app = express();
@@ -18,12 +22,19 @@ mongoose
 // apply middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use(morgan("dev"));
 
 // route
 readdirSync("./routes").map(async (r) => {
     const route = await import(`./routes/${r}`);
     app.use("/api", route.default);
+});
+// csrf
+app.use(csrfProtection);
+
+app.get("/api/csrf-token", (req, res) => {
+    res.json({ csrfToken: req.csrfToken() });
 });
 
 // port
